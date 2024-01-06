ed633f375836944fe00579935b0fe9e94a253151
TIGER <141488144+tiger7815@users.noreply.github.com>
2024-01-06 10:47:39 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 22609670
 api_hash = "3506d8474ad1f4f5e79b7c52a5c3e88d"
-bot_token = "6711741427:AAGrYQvfpms_7_wIIvHfnOWzUwkQF_2uKlM"
+bot_token = "6502851708:AAEqGci9nnyLMHaYk6SxkvRgtp1DO_rBZa0"
 
